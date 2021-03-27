0db801603de45ae5d69269bb09f111d1684ee725
Jayden <jayden@jaydensar.net>
2021-03-26 22:05:16 -0400

    document api

--- sidebars.js
+++ sidebars.js
@@ -2,5 +2,6 @@ module.exports = {
 	sidebar: {
 		RoManager: ['RoManager/introduction', 'RoManager/gettingStarted'],
 		Guides: ['Guides/inGame', 'Guides/permissionOverrides'],
+		API: ['API/gettingStarted', 'API/group'],
 	},
 };
