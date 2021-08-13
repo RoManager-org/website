7c70dec1179c9fd4a38a202f4998ea2b9ce63482
Jayden <jayden@jaydensar.net>
2021-08-13 00:21:11 -0400

    docs(in-game): remove game integration gude

--- sidebars.js
+++ sidebars.js
@@ -2,7 +2,6 @@ module.exports = {
 	sidebar: {
 		RoManager: ['RoManager/introduction', 'RoManager/gettingStarted'],
 		Guides: [
-			'Guides/inGame',
 			'Guides/permissionOverrides',
 			'Guides/customBotAccount',
 		],
