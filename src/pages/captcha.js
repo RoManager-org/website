625516a1bcdc002bde1b06c2245dfc7efe9d50b1
jaydensar <jayden@jaydensar.net>
2022-01-08 01:03:35 -0500

    fix: add URLSearchParams import

--- src/pages/captcha.js
+++ src/pages/captcha.js
@@ -1,6 +1,7 @@
 import Layout from "@theme/Layout";
 import PropTypes from "prop-types";
 import React from "react";
+import { URLSearchParams } from "url";
 
 function Home(props) {
 	const frameUrl = `https://captcha.romanager.bot/?taskId=${new URLSearchParams(
